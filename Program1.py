766545dd39f679068614e51c37c07d7d9612d281
Gector113 <90830467+Gector113@users.noreply.github.com>
2021-09-23 18:31:10 +0300

    Add files via upload

--- Program1.py
+++ Program1.py
@@ -1,5 +1,13 @@
-print('Daniil Avramenko')
-print('0960183325')
-print('Сathedral street 10, apartment 25, Fastiv')
-print('08500')
-print('Ukraine')
+try:
+    name = str(input('your name - '))
+    surname = str(input('your surname - '))
+    PhoneNumber = int(input('your phone number - '))
+    street = str(input('your name street -'))
+    HomeNumber = int(input('your home number - '))
+    FlatNumber = int(input('your flat number - '))
+    CityName = str(input('your city name - '))
+    Index = int(input('your index - '))
+    Country = str(input('your country - '))
+except:
+    print('Введені не правильні значення')
+    
